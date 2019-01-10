4ddc09f2ad32b6ef50f0a39a07fdf28d9e6552f1
Matthias Lischka <matthias.lischka@gmx.at>
2019-01-10 16:11:11 +0100

    add pancake sorter

---
+++ PancakeSort/Sorter.cs
@@ -0,0 +1,33 @@
+﻿namespace PancakeSort
+{
+    public class Sorter
+    {
+        Flipper flipper = new Flipper();
+
+        public int[] Sort(int[] arr)
+        {
+            if (arr.Length < 2)
+                return arr;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    while (arr[i] < arr[j])
+                    {
+                        arr = MoveToPosition(arr, j, i);
+                        j = i - 1;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public int[] MoveToPosition(int[] arr, int startPos, int targetPos)
+        {
+            var flippedToStartPos = flipper.Flip(arr, startPos + 1);
+            return flipper.Flip(flippedToStartPos, targetPos + 1);
+        }
+    }
+}
---
+++ PancakeSort/SorterTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PancakeSort
+{
+    public class SorterTests
+    {
+        [Fact]
+        public void EmptyArray_ShouldReturnEmptyArray()
+        {
+            new Sorter().Sort(new int[0]).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void SortedArray_ShouldReturnUnchanged()
+        {
+            AssertSort(new[] { 1, 2, 3 }, new[] { 1, 2, 3 });
+        }
+
+        [Fact]
+        public void UnsortedTwoElementsArray_ShouldReturnSorted()
+        {
+            AssertSort(new[] { 2, 1 }, new[] { 1, 2 });
+        }
+
+        [Theory]
+        [InlineData(new[] { 2, 1, 3 }, new[] { 1, 2, 3 })]
+        [InlineData(new[] { 3, 2, 1 }, new[] { 1, 2, 3 })]
+        [InlineData(new[] { 5, 8, 1, 9, 4, 3, 2, 6, 7 }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        [InlineData(new[] { 1, 7, 6, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5, 6, 7 })]
+        public void Sort_ShouldReturnSorted(int[] arr, int[] expectedArray)
+        {
+            AssertSort(arr, expectedArray);
+        }
+
+        private void AssertSort(int[] arr, int[] expectedArray)
+        {
+            var actualResult = new Sorter().Sort(arr);
+            actualResult.Should().BeEquivalentTo(expectedArray, c => c.WithStrictOrdering(), $"actual result: {string.Join(", ", actualResult)}");
+
+        }
+    }
+}
